b2e74c62247d7735b40e1784e178a54d8cc97d2e
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-05-22 12:18:13 -0300

    Initial Commit

---
+++ twitter-app-view-code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twitter-app-view-code
+//
+//  Created by João Alexandre on 22/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
