e3ceba2474c32b294c766bc027cf85df65c9362e
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-05-24 01:53:35 -0300

    added background and logo

--- twitter-app-view-code/Controller/FeedController.swift
+++ twitter-app-view-code/Controller/FeedController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class FeedController: UIViewController {
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .brown
+        configureUI()
+    }
+    
+    //MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        
+        let imageView = UIImageView(image: UIImage(named: "twitter-logo"))
+        imageView.contentMode = .scaleAspectFit
+        navigationItem.titleView = imageView
     }
 }
