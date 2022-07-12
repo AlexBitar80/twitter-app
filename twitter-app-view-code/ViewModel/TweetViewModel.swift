ff2a8a9acfc2a31c11f9210fbe4a14d46789eff8
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-07-11 21:24:42 -0300

    added logic for difference between user info and tweet post count

---
+++ twitter-app-view-code/ViewModel/TweetViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  TweetViewModel.swift
+//  twitter-app-view-code
+//
+//  Created by João Alexandre on 11/07/22.
+//
+
+import UIKit
+
+class TweetViewModel {
+	let tweet: Tweet
+	let user: User
+
+	var profileImageUrl: URL? {
+		return user.profileImageUrl
+	}
+
+	var timestamp: String {
+		let formatter = DateComponentsFormatter()
+		formatter.allowedUnits = [.second, .minute, .hour, .day, .weekOfMonth]
+		formatter.maximumUnitCount = 1
+		formatter.unitsStyle = .abbreviated
+		let now = Date()
+		return formatter.string(from: tweet.timestamp ?? Date(), to: now) ?? "2m"
+	}
+
+	var userInfoText: NSAttributedString {
+		let title = NSMutableAttributedString(
+			string: user.fullname,
+			attributes: [
+				.font: UIFont.systemFont(ofSize: 14, weight: .bold)
+			]
+		)
+
+		title.append(NSAttributedString(
+			string: " @\(user.username)",
+			attributes: [
+				.font: UIFont.systemFont(ofSize: 14),
+				.foregroundColor: UIColor.lightGray
+			]
+		))
+
+		title.append(NSAttributedString(
+			string: " ・ \(timestamp)",
+			attributes: [
+				.font: UIFont.systemFont(ofSize: 14),
+				.foregroundColor: UIColor.lightGray
+			]
+		))
+
+		return title
+	}
+
+	init(tweet: Tweet) {
+		self.tweet = tweet
+		self.user = tweet.user
+	}
+}
