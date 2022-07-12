6d9c81729fa50f073abd28f74ff5a1a1aadff9dc
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-07-11 21:13:51 -0300

    fetching firebase username

--- twitter-app-view-code/Model/User.swift
+++ twitter-app-view-code/Model/User.swift
@@ -10,12 +10,14 @@ import Foundation
 struct User {
 	let uid: String
 	let email: String
+	let fullname: String
 	let username: String
 	var profileImageUrl: URL?
 
 	init(uid: String, dictionary: [String: AnyObject]) {
 		self.uid = uid
 		self.email = dictionary["email"] as? String ?? ""
+		self.fullname = dictionary["fullname"] as? String ?? ""
 		self.username = dictionary["username"] as? String ?? ""
 
 		if let profileImageUrlString = dictionary["profileImageUrl"] as? String {
