dc2f0b0d78b1600412a747c3669cb9a1da499311
joão Alexandre Bitar <joaoalexbitar@brq.com>
2022-09-05 12:45:06 -0300

    registering the table cell and displaying the registered users

--- twitter-app-view-code/Controller/ExploreController.swift
+++ twitter-app-view-code/Controller/ExploreController.swift
@@ -7,21 +7,72 @@
 
 import UIKit
 
-class ExploreController: UIViewController {
+class ExploreController: UITableViewController {
+    // MARK: - Properties
+
+    var users: [User] = [] {
+        didSet { tableView.reloadData() }
+    }
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureUI()
+        fetchUsers()
     }
+
+    // MARK: - API
+
+    func fetchUsers() {
+        UserService.shared.fetchUsers { users in
+            self.users = users
+        }
+    }
+
+    // MARK: - Helpers
 
     func configureUI() {
 		let appearance = UINavigationBarAppearance()
 		appearance.configureWithOpaqueBackground()
+
+        tableView.register(
+            UserCell.self,
+            forCellReuseIdentifier: UserCell.reuseIdentifier
+        )
+        tableView.rowHeight = 60
+        tableView.separatorStyle = .none
 
 		navigationController?.navigationBar.isTranslucent = false
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
         navigationItem.title = "Explore"
 
         view.backgroundColor = .white
+    }
+}
+
+// MARK: - UITableViewControllerDelegate
+extension ExploreController {
+    override func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return users.count
+    }
+
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: UserCell.reuseIdentifier,
+            for: indexPath
+        ) as? UserCell else {
+            return UserCell()
+        }
+
+        cell.user = users[indexPath.row]
+        return cell
     }
 }
